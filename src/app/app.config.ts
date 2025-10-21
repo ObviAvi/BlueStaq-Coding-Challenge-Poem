30d56cae53071ef479488f777ee2ed877f3b641f
ObviAvi <aggarwal.avi@gmail.com>
2025-10-20 23:31:51 -0400

    UI updates

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,13 +3,11 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-    importProvidersFrom(FormsModule),   // ✅ replaces provideForms()
-    provideAnimationsAsync()            // ✅ replaces deprecated provideAnimations()
+    importProvidersFrom(FormsModule),
   ]
 };
